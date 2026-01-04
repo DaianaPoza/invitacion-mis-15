5e9dd336ab80322fe5bb85ee8523454cc7e92270
Daiana Poza <daianapoza@gmail.com>
2026-01-04 19:43:53 -0300

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,8 @@ const repo = "invitacion-mis-15";
 export default defineConfig({
   plugins: [react()],
   base: `/${repo}/`,      // importante para que carguen los assets en Pages
-  build: { outDir: "docs" } // generamos el build en /docs
+  build: { 
+    outDir: "docs",
+    emptyOutDir: true,
+  }, // generamos el build en /docs
 });
